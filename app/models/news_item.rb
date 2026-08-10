dcc99e1d7e3187840e70108b097046b56e834593
Yunxiang Mo <m2556377578@gmail.com>
2026-08-10 15:37:48 -0700

    [feat] Task 2.1: add Issue column to news articles
    Add an issue string column to news_items and a NewsItem.issues helper
    returning the fixed 17-issue list that populates the dropdown on the
    Create News Article page. Model specs cover the list and persistence.

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -21,6 +21,19 @@ class NewsItem < ApplicationRecord
   # TODO: this belongs to a user (creator_id)
   belongs_to :representative
 
+  # The fixed list of political issues an article can be associated with.
+  # Populates the "Issue" dropdown on the Create News Article page (Task 2.1).
+  ISSUES = [
+    'Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare',
+    'Abortion', 'Student Loans', 'Gun Control', 'Unemployment', 'Climate Change',
+    'Homelessness', 'Racism', 'Tax Reform', 'Net Neutrality', 'Religious Freedom',
+    'Border Security', 'Minimum Wage', 'Equal Pay'
+  ].freeze
+
+  def self.issues
+    ISSUES
+  end
+
   def self.find_for(representative_id)
     NewsItem.find_by(
       representative_id: representative_id
